bea0a7753182245b1337ec58326899685da0b7e0
andrewsa123 <andrewknott_123@hotmail.co.uk>
2018-11-30 13:27:44 +0000

    Cors

--- src/main/java/com/qa/rest/AccountEndpoint.java
+++ src/main/java/com/qa/rest/AccountEndpoint.java
@@ -1,6 +1,7 @@
 package com.qa.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import com.qa.service.AccountNumberGen;
 
 @RequestMapping("${base_endpoint}")
 @RestController
+@CrossOrigin
 public class AccountEndpoint {
 
 	@Autowired
